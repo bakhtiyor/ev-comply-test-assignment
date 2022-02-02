14c16151e065ee4681696feed5a85965eca96dc0
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2022-02-02 23:30:15 +0500

    Change main page from a welcome page to a subscription page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('subscription/create', [SubscriptionController::class, 'create'])->name('subscription-create');
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SubscriptionController::class, 'create'])->name('subscription-create');
